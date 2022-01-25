9e0573d18e9b269c89fa2a0ea15328df7ce3142f
nguy11 <duc11.nguyenvan@nttdata.com>
2022-01-26 00:54:15 +0700

    - Fix does not found Coder for Multiple Output - Change TransformName

--- src/main/java/org/example/gcloud/pubsub/io/WriteToPubSub.java
+++ src/main/java/org/example/gcloud/pubsub/io/WriteToPubSub.java
@@ -12,7 +12,7 @@ import org.checkerframework.checker.nullness.qual.NonNull;
  */
 public class WriteToPubSub extends PTransform<PCollection<PubsubMessage>, PDone> {
 
-    private static final String TRANSFORM_NAME = "Write To Big Query";
+    private static final String TRANSFORM_NAME = "Write To Pub Sub";
 
     private final PubsubIO.Write<PubsubMessage> writeTo;
 
