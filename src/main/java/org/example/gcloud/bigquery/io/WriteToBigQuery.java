081d5c823486337ee90a2037cb7dc8db0d05df5f
nguy11 <duc11.nguyenvan@nttdata.com>
2022-02-03 21:27:11 +0700

    - Add Document

--- src/main/java/org/example/gcloud/bigquery/io/WriteToBigQuery.java
+++ src/main/java/org/example/gcloud/bigquery/io/WriteToBigQuery.java
@@ -2,16 +2,18 @@ package org.example.gcloud.bigquery.io;
 
 import com.google.api.services.bigquery.model.TableRow;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
-import org.apache.beam.sdk.io.gcp.bigquery.InsertRetryPolicy;
 import org.apache.beam.sdk.io.gcp.bigquery.WriteResult;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.SerializableFunction;
-import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.values.PCollection;
 
 /**
- * Write to BigQuery Table
+ * Write to BigQuery Table directly, instead of using the {@link TableRow}.
+ * This transformation has better performance overhead, compare to
+ * {@link WriteTableRowToBigQuery} because the {@link TableRow} is created just
+ * before the data is pushed to BigQuery and will not be re-used.
  *
+ * @see <a href='https://github.com/apache/beam/pull/4694'>This Apache Beam Pull Request<a/>
  * @param <T> the type of data which is written to BigQuery
  */
 public class WriteToBigQuery<T> extends PTransform<PCollection<T>, WriteResult> {
